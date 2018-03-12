c8b2103212186e1290a59408ff5eeea0a75db449
Kevin Solorzano <solorzke@gmail.com>
2018-03-12 18:36:31 -0400

    Imported libraries and pulled page text from html file

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ import ssl
 import os
 import nltk
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import codecs
+
+
+        
+f = codecs.open("F:\GitHub\WebCrawler\web_crawler\crawled_html_pages/3D audio effect - Wikipedia.html", 'r', 'utf-8')
+
+soup = BeautifulSoup(f, 'html.parser')
+page_text = soup.get_text()
+
+
+
+#print(page_text)
