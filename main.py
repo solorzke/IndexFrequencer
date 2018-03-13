11697da645711237154326f11a528cd4f2af6d27
Kevin Solorzano <solorzke@gmail.com>
2018-03-13 16:19:47 -0400

    Built Indexer

--- main.py
+++ main.py
@@ -4,42 +4,51 @@ import re
 import ssl
 import os
 import nltk
-
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import codecs
 
+def checkChar(token):
+    for char in token:
+        if (0 <= ord(char) and ord(char) <= 64) or (91 <= ord(char) and ord(char) <= 96) or (123 <= ord(char)):
+           return False
+        else:
+            continue
+    return True
 
-        
-f = codecs.open("F:\GitHub\WebCrawler\web_crawler\crawled_html_pages/3D audio effect - Wikipedia.html", 'r', 'utf-8')
-
-soup = BeautifulSoup(f, 'html.parser')
-page_text = soup.get_text()
-stop_words = set(stopwords.words('english'))
-
+path = 'F:\GitHub\WebCrawler\web_crawler\crawled_html_pages'
 index = {}
 docNum = 0
+for filename in os.listdir(path):                                                                       #for every doc in folder...
+    collection = {}                                                                                     #all words collected from this html file
+    docNum += 1                                                                                         #Start with doc 1, then 2..3..4..500
+    file = codecs.open('F:\GitHub\WebCrawler\web_crawler\crawled_html_pages\\'+ filename,'r', 'utf-8')  #open the html file
+    soup = BeautifulSoup(file, 'html.parser')                                                           #parse the html
+    page_text = soup.get_text()                                                                         #collect page text from html file
+    stop_words = set(stopwords.words('english'))                                                        #collection of stopwords to avoid       
+    tokens = nltk.word_tokenize(page_text)                                                              #tokenize page text
+    filtered_sentence = [w for w in tokens if not w in stop_words]                                      #remove stopwords from page text
+    filtered_sentence = []
 
-c = {}
-tokens = nltk.word_tokenize(page_text)
+    for w in tokens:                                                                                    #place non-stopwords in token list                                                                                   
+        if w not in stop_words:
+            filtered_sentence.append(w.lower())
 
-filtered_sentence = [w for w in tokens if not w in stop_words]
-filtered_sentence = []
+    for token in filtered_sentence:
+        if checkChar(token) == False:
+            continue
+        if token not in collection:
+            collection[token] = 0
+        collection[token] += 1            
 
-for w in tokens:
-    if w not in stop_words:
-        filtered_sentence.append(w)
-
-test = {}
-for token in filtered_sentence:
-    if not token in test:
-        test[token] = 0
-    test[token] += 1
-    
-
-for token in test:
-    if token not in index:
-        index[token] = test[token]
-
-print(index)
-#print(page_text)
+    for token in collection:
+        if token not in index:
+            index[token] = ''
+        index[token] = index[token] + '['+ str(docNum) + ', ' + str(collection[token]) + ']'
+    if docNum == 3:
+        break
+    else:
+        continue            
+        
+for k in index:
+    print(k + ' '+ index[k])
--- testing.py
+++ testing.py
@@ -8,10 +8,29 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import codecs
 
+strw = ' 1234Hello'
+str1 = 'str:d' + strw
 
-for filename in os.listdir('F:\GitHub\WebCrawler\web_crawler\crawled_html_pages'):
-    file = codecs.open('F:\GitHub\WebCrawler\web_crawler\crawled_html_pages\\'+ filename,'r', 'utf-8')
-    soup = BeautifulSoup(file, 'html.parser')
-    title = soup.title.string
-    print(title)
+tokens = nltk.word_tokenize(str1)
+'''
+for token in tokens:
+    for char in token:
+        if (0 <= ord(char) and ord(char) <= 64) or (91 <= ord(char) and ord(char) <= 96) or (123 <= ord(char) and ord(char) <= 126):
+           print(char)
+           break
+        else:
+            print(char)
+            continue
+    break
 
+print(ord('s'))
+'''
+def checkChar(token):
+    for char in token:
+        if (0 <= ord(char) and ord(char) <= 64) or (91 <= ord(char) and ord(char) <= 96) or (123 <= ord(char) and ord(char) <= 126):
+           return False
+        else:
+            continue
+    return True
+
+print(checkChar(str1))
