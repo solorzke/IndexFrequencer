d6d037594d329e37be0e369fac58800bac22df4e
Kevin Solorzano <solorzke@gmail.com>
2018-03-12 21:33:02 -0400

    Tokenized, filtered stop-words, and indexed words

--- main.py
+++ main.py
@@ -15,7 +15,31 @@ f = codecs.open("F:\GitHub\WebCrawler\web_crawler\crawled_html_pages/3D audio ef
 
 soup = BeautifulSoup(f, 'html.parser')
 page_text = soup.get_text()
+stop_words = set(stopwords.words('english'))
 
+index = {}
+docNum = 0
 
+c = {}
+tokens = nltk.word_tokenize(page_text)
 
+filtered_sentence = [w for w in tokens if not w in stop_words]
+filtered_sentence = []
+
+for w in tokens:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+
+test = {}
+for token in filtered_sentence:
+    if not token in test:
+        test[token] = 0
+    test[token] += 1
+    
+
+for token in test:
+    if token not in index:
+        index[token] = test[token]
+
+print(index)
 #print(page_text)
