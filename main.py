366ba38be0868862180400319933dad5cca2d4e6
Kevin Solorzano <solorzke@gmail.com>
2018-03-13 21:30:54 -0400

    Stripped special characters and wrote indices in text files

--- main.py
+++ main.py
@@ -16,27 +16,50 @@ def checkChar(token):
             continue
     return True
 
+def cleanMe(html):
+    soup = BeautifulSoup(html, "html.parser")                                                       # create a new bs4 object from the html data loaded
+    for script in soup(["script", "style"]):                                                        # remove all javascript and stylesheet code
+        script.extract()
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+
 path = 'F:\GitHub\WebCrawler\web_crawler\crawled_html_pages'
 index = {}
 docNum = 0
+stop_words = set(stopwords.words('english'))                                                            #collection of stopwords to avoid       
 for filename in os.listdir(path):                                                                       #for every doc in folder...
     collection = {}                                                                                     #all words collected from this html file
     docNum += 1                                                                                         #Start with doc 1, then 2..3..4..500
     file = codecs.open('F:\GitHub\WebCrawler\web_crawler\crawled_html_pages\\'+ filename,'r', 'utf-8')  #open the html file
-    soup = BeautifulSoup(file, 'html.parser')                                                           #parse the html
-    page_text = soup.get_text()                                                                         #collect page text from html file
-    stop_words = set(stopwords.words('english'))                                                        #collection of stopwords to avoid       
+    page_text = cleanMe(file)                                                                         #collect page text from html file
     tokens = nltk.word_tokenize(page_text)                                                              #tokenize page text
     filtered_sentence = [w for w in tokens if not w in stop_words]                                      #remove stopwords from page text
     filtered_sentence = []
+
+    breakWord = ''
 
     for w in tokens:                                                                                    #place non-stopwords in token list                                                                                   
         if w not in stop_words:
             filtered_sentence.append(w.lower())
 
     for token in filtered_sentence:
+        if len(token) == 1 or token == 'by':
+            continue
         if checkChar(token) == False:
             continue
+        if token == 'australia':
+            breakWord = token
+            continue
+        if token == 'bangladesh' and breakWord == 'australia':
+            break
         if token not in collection:
             collection[token] = 0
         collection[token] += 1            
@@ -45,10 +68,18 @@ for filename in os.listdir(path):                                               
         if token not in index:
             index[token] = ''
         index[token] = index[token] + '['+ str(docNum) + ', ' + str(collection[token]) + ']'
-    if docNum == 3:
+    if docNum == 2:
         break
     else:
         continue            
-        
-for k in index:
-    print(k + ' '+ index[k])
+
+f = open('index.txt', 'w')
+vocab = open('uniqueWords.txt', 'w')
+for term in index:
+    f.write(term + ' => ' + index[term])
+    vocab.write(term + '\n')
+    f.write('\n')
+f.close()
+vocab.close()
+
+print('Finished...')
--- testing.py
+++ testing.py
@@ -8,29 +8,28 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import codecs
 
-strw = ' 1234Hello'
-str1 = 'str:d' + strw
 
-tokens = nltk.word_tokenize(str1)
-'''
-for token in tokens:
-    for char in token:
-        if (0 <= ord(char) and ord(char) <= 64) or (91 <= ord(char) and ord(char) <= 96) or (123 <= ord(char) and ord(char) <= 126):
-           print(char)
-           break
-        else:
-            print(char)
-            continue
+
+
+
+def cleanMe(html):
+    soup = BeautifulSoup(html, "html.parser") # create a new bs4 object from the html data loaded
+    for script in soup(["script", "style"]): # remove all javascript and stylesheet code
+        script.extract()
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+
+path = 'F:\GitHub\WebCrawler\web_crawler\crawled_html_pages'
+for filename in os.listdir(path):                                                                       #for every doc in folder...
+    file = codecs.open('F:\GitHub\WebCrawler\web_crawler\crawled_html_pages\\'+ filename,'r', 'utf-8')  #open the html file
+    cleaned = cleanMe(file)                                                                  #collect page text from html file
+    print(cleaned)
     break
-
-print(ord('s'))
-'''
-def checkChar(token):
-    for char in token:
-        if (0 <= ord(char) and ord(char) <= 64) or (91 <= ord(char) and ord(char) <= 96) or (123 <= ord(char) and ord(char) <= 126):
-           return False
-        else:
-            continue
-    return True
-
-print(checkChar(str1))
